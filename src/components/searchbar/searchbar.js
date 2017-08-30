67996c49f0d8ff25bf2c7465c82ae89d719b13e4
Inna <designbyinna@gmail.com>
2017-08-30 10:12:03 -0600

    error

---
+++ src/components/searchbar/searchbar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './searchbar.css';
+
+let sortByOptions = {
+  'Best Match':'best-match',
+  'Highest Rated':'rating',
+  'Most Reviewed':'review_count',
+}
+
+class SearchBar extends React.Component{
+  renderSortByOptions()
+  return
+  object.keys(sortByOptions).map(sortByOption => {
+  let sortByOptionValue = sortByOptions[sortByOption];
+  return <li key={sortByOptionValue}> {sortByOption} </li>
+
+  render(){
+    <div className="SearchBar">
+  <div className="SearchBar-sort-options">
+    <ul>
+    renderSortByOptions()
+    </ul>
+  </div>
+  <div className="SearchBar-fields">
+    <input placeholder="Search Businesses" />
+    <input placeholder="Where?" />
+  </div>
+  <div className="SearchBar-submit">
+    <a>Lets Go </a>
+  </div>
+</div>
+  }
+  });
+}
+
+export default SearchBar;
